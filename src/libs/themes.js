522a93d2c9731ec07d5c3fd4f351c5d49821bfe5
Nathan Quach <nathanquach8x@gmail.com>
2019-01-23 00:48:40 +1100

    Refactor theme

--- src/libs/themes.js
+++ src/libs/themes.js
@@ -33,7 +33,7 @@ export default {
       onSurface: colors.grey[800],
       onError: 'white',
       // border
-      border: colors.grey[400]
+      border: colors.grey[300]
     },
     ...common
   },
